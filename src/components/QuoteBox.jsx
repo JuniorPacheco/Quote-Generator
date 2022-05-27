7ec2c61a5ff1afb87d9caf10c1713d926374aa35
Junior Pacheco <junior.pacheco.356@gmail.com>
2022-05-26 23:33:43 -0500

    Primer commit

--- src/components/QuoteBox.jsx
+++ src/components/QuoteBox.jsx
@@ -6,21 +6,33 @@ const Quote = ({randomQuote, randomColor, color, setColor}) => {
 
   return (
     <article className="quoteBox">
-        <QuoteText quoteText={quoteText.quote} color={color}/>
+      <QuoteText quoteText={quoteText.quote} color={color}/>
 
-    <div className="container-quoteAndAuthor">
-      <div>
-          <p style={{color: color}}>{quoteText.author}</p>
-      </div>
+      <div className="containerQuote">
 
-      <div 
+        <div 
+        className="quoteMarks" 
+        style={{color: color}}
+        >
+          <i className='bx bxs-quote-left'></i>
+        </div>
+
+        <div className="containerQuote__authorAndButton">
+
+          <div>
+            <p style={{color: color}}>{quoteText.author}</p>
+          </div>
+
+          <div 
           className="buttonNextQuote" 
           style={{backgroundColor: color}}
           onClick={()=>{setColor(randomColor()); setQuoteText(randomQuote())}}
           >
             <i className='bx bx-chevron-right'></i>
-      </div>
-    </div>
+          </div>
+        </div>
+
+        </div>
 
     </article>
   )
